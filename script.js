823a497afea4e97280ffce00a1d1a03c5242e121
Ulathi Ncube <swiftly.graphics@gmail.com>
2025-07-06 10:29:35 +0200

    Feat: Create function playGame

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// Define global scores of User and Computer
-
-let humanScore = 0;
-let computerScore = 0;
-
 // Function to get ComputerChoice
 
 function getComputerChoice() {
@@ -23,33 +18,46 @@ function getHumanChoice() {
   return result;
 }
 
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-
 // Function to play a Round
 // (" rock > scissors, scissors > paper, paper > rock ")
-function playRound(humanChoice, computerChoice) {
-  const playerWins =
-    (humanChoice === 'rock' && computerChoice === 'scissors') ||
-    (humanChoice === 'paper' && computerChoice === 'rock') ||
-    (humanChoice === 'scissors' && computerChoice === 'paper');
 
-  const computerWins =
-    (computerChoice === 'rock' && humanChoice === 'scissors') ||
-    (computerChoice === 'paper' && humanChoice === 'rock') ||
-    (computerChoice === 'scissors' && humanChoice === 'paper');
+// Function to play game for 5 rounds
 
-  // const tie = humanChoice === computerChoice;
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
 
-  if (playerWins) {
-    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-    humanScore++;
-  } else if (computerWins) {
-    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-    computerScore++;
-  } else {
-    console.log(`It's a tie. You both picked ${humanChoice || computerChoice}`);
+  function playRound(humanChoice, computerChoice) {
+    const playerWins =
+      (humanChoice === 'rock' && computerChoice === 'scissors') ||
+      (humanChoice === 'paper' && computerChoice === 'rock') ||
+      (humanChoice === 'scissors' && computerChoice === 'paper');
+
+    const computerWins =
+      (computerChoice === 'rock' && humanChoice === 'scissors') ||
+      (computerChoice === 'paper' && humanChoice === 'rock') ||
+      (computerChoice === 'scissors' && humanChoice === 'paper');
+
+    // const tie = humanChoice === computerChoice;
+
+    if (playerWins) {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+      humanScore++;
+    } else if (computerWins) {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      computerScore++;
+    } else {
+      console.log(
+        `It's a tie. You both picked ${humanChoice || computerChoice}`
+      );
+    }
+  }
+
+  for (let game = 0; game < 5; game++) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
   }
 }
 
-playRound(humanChoice, computerChoice);
+playGame();
